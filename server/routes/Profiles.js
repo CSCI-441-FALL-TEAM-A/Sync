5e9f1190b805fc86f6bd40276989c84b7b1b92e1
Ryoung27 <ryoung@gmail.com>
2024-10-29 19:57:31 -0500

    update profile for get all profiles and get profile by user id

--- server/routes/Profiles.js
+++ server/routes/Profiles.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const {getProfileById, createProfile, updateProfile, deleteProfile} = require('../controllers/profileController');
+const {getProfileById, createProfile, updateProfile, deleteProfile, getProfileByUserId, getAllProfiles} = require('../controllers/profileController');
 const router = express.Router();
 
 //Route to get profile by id
 router.get('/:id', getProfileById);
+//Route to get profile by user_id
+router.get('/user/:user_id', getProfileByUserId);
+// Route to get all profiles
+router.get('/', getAllProfiles);
 //Route to add profile
 router.post('/', createProfile);
 //Route to update profile
